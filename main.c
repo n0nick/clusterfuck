09a344ec47a72278993ef95c8972aa425ee2147b
n0nick <sagiem@gmail.com>
2012-08-11 17:17:31 +0300

    Parsing and validating args.

--- main.c
+++ main.c
@@ -6,13 +6,42 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "node.h"
-
+#include "files.h"
 
 /* Global variables */
+node* nodes = NULL;
+edge* edges = NULL;
 
 /* main */
 int main(int argc, char* argv[]) {
-	printf("Deep");
+	char *inputFolder;
+	char *outputFolder;
+	int lowerBound;
+	int upperBound;
+
+	/* parse arguments */
+	if (argc != 5) {
+		perror("Usage: kcluster <inputFolder> <outputFolder> <lowerBound> <upperBound>\n");
+		return 1;
+	}
+	inputFolder = argv[1];
+	outputFolder = argv[2];
+	lowerBound = atoi(argv[3]);
+	upperBound = atoi(argv[4]);
+
+	/* validate arguments */
+	if (lowerBound == 0 || upperBound == 0) {
+		perror("Error: cluster size bounds should be positive integers.\n");
+		return 1;
+	}
+	if (lowerBound > upperBound) {
+		perror("Error: lower bound should be lower than upper bound.\n");
+		return 1;
+	}
+
+	/*TODO debug*/
+	printf("in: %s\nout: %s\nlow: %d up: %d\n", inputFolder, outputFolder, lowerBound, upperBound);
 	return 0;
 }
