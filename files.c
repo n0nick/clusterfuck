dd80f2278d033b8acca60ec476b5df7d994f2185
n0nick <sagiem@gmail.com>
2012-08-11 17:18:28 +0300

    Implementing read_data().

--- files.c
+++ files.c
@@ -5,13 +5,23 @@
  *      Author: sagiemao
  */
 
+#include <string.h>
+#include "consts.h"
+#include "node.h"
+#include "files.h"
 
+/* Methods */
 int read_data(char* inputFolder) {
-	return 0;
+	bool success;
+	success = read_nodes(strcat(inputFolder, "nodes"));
+	success = read_edges(strcat(inputFolder, "edges")) && success;
+	return success;
 }
 int read_nodes(char* nodesPath) {
+	extern node* nodes;
 	return 0;
 }
 int read_edges(char* edgesPath) {
+	extern edge* edges;
 	return 0;
 }
