3385ebe736cca4bbfd158019ac8619490f4161f3
n0nick <sagiem@gmail.com>
2012-08-11 17:17:50 +0300

    Removing old print_* functions.

--- node.h
+++ node.h
@@ -1,12 +1,7 @@
 #ifndef nodeFUCK
 #define nodeFUCK
 
-#define VERTEX_ARRAY_INITIAL_SIZE 2
 #define NODE_ARRAY_SIZE_FACTOR 2
-
-#define MAX_nodes 100
-#define MAX_WEIGHT 100
-#define MAX_TOTAL_WEIGHTS 1000
 
 typedef struct edge edge;
 typedef struct node node;
@@ -26,11 +21,6 @@ struct node {
 };
 
 node* add_node(node* currentArray, int* size, int* maxSize, char* name, int* success);
-void print_nodes(node* nodes, int size, int* success);
-void print_edges(node* nodes, int size, int* success);
-void print_degree(node* nodes, int id, int* success);
-void print_by_name(node* nodes, char* name, int size, int* success);
-
 void add_edge(node* nodes, int id1, int id2, double weight, int* countEdges, double* totalWeights, int* success);
 void remove_edge(node* nodes, int id1, int id2, int* countEdges, double* totalWeights, int* success);
 void add_one_edge(node* nodeFrom, node* nodeTo, double weight, int* success);
