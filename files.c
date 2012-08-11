17356d3d2979a3846473215f235aa1adc49882d4
n0nick <sagiem@gmail.com>
2012-08-11 18:16:39 +0300

    A working read_nodes().

--- files.c
+++ files.c
@@ -5,6 +5,8 @@
  *      Author: sagiemao
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "consts.h"
 #include "node.h"
@@ -19,7 +21,41 @@ bool read_data(char* inputFolder) {
 }
 bool read_nodes(char* nodesPath) {
 	extern node* nodes;
-	return 0;
+
+	FILE * fp;
+	char ch;
+	int count = 0;
+	int i = 0;
+	char name [MAX_LINE_LENGTH];
+
+	/* open file */
+	fp = fopen(nodesPath, "r");
+	if (fp == NULL) {
+		perror("Error: Input file not readable.\n");
+		return FALSE;
+	}
+
+	/* figure out number of nodes in file */
+	ch = getc(fp);
+	while (ch != EOF) {
+		if (ch == '\n') {
+			count++;
+		}
+		ch = getc(fp);
+	}
+
+	/* Initialize nodes array */
+	nodes = (node*) malloc(count * sizeof(node));
+
+	/* Loop through lines, adding nodes */
+	rewind(fp);
+	while (fscanf(fp, "protein: %s\n", name) == 1) {
+		add_node(i++, name);
+	}
+
+	print_nodes(nodes, count);
+
+	return TRUE;
 }
 bool read_edges(char* edgesPath) {
 	extern edge* edges;
