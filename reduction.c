f83757f3bfd3995e271d355d04fdbc8c48cc5dc4
Amir Moualem <amoualem@gmail.com>
2012-08-17 17:09:27 +0300

    Adding work in progress lp_matrix, lp_rhs_sense

--- reduction.c
+++ reduction.c
@@ -47,3 +47,72 @@ bool lp_objective_function_coefficients(int k, double** coefficients) {
 
 	return TRUE;
 }
+
+bool lp_rhs_sense(int k, double** rhs, char** sense) {
+	extern node* nodes;
+	extern int nodesCount;
+	extern int edgesCount;
+
+	int i;
+
+	/* constraint 1 */
+	for (i=0; i<(2*edgesCount*k); i++) {
+		rhs[i] = 0;
+		sense[i] = 'L';
+	}
+
+	/* constraint 2 */
+	for (; i<(3*edgesCount*k); i++) {
+		rhs[i] = -1;
+		sense[i] = 'G';
+	}
+
+	/* constraint 3 */
+	for (; i<(3*edgesCount*k + nodesCount); i++) {
+		rhs[i] = 1;
+		sense[i] = 'E';
+	}
+
+	/* contraint 4 */
+	for (; i<(3*edgesCount*k + nodesCount + k); i++) {
+		rhs[i] = 1;
+		sense[i] = 'G';
+	}
+
+	return TRUE;
+}
+
+bool lp_matrix(int k, int *matbeg, int *matcnt, int *matind, double *matval) {
+	extern node* nodes;
+	extern int nodesCount;
+	extern int edgesCount;
+
+	int currRow = 0;
+
+	int i;
+	int cluster;
+	int nodeIndex;
+	edge* currEdge;
+
+	/* constraint 1 */
+	for (cluster=0; cluster<k; cluster++) {
+		for (nodeIndex = 0; nodeIndex < nodesCount; nodeIndex++) {
+			currEdge = nodes[nodeIndex].edges;
+			while (currEdge != NULL) {
+
+				matavl.push(-1);
+				matind.push(currRow);
+
+
+				currEdge = currEdge->next;
+				currRow++;
+			}
+		}
+	}
+
+	/* constraint 2 */
+
+	/* constraint 3 */
+
+	/* constraint 4 */
+}
--- reduction.h
+++ reduction.h
@@ -4,5 +4,7 @@
 #include "consts.h"
 
 bool lp_objective_function_coefficients(int k, double** coefficients);
+bool lp_rhs_sense(int k, double** rhs, char** sense);
+bool lp_matrix(int k, int *matbeg, int *matcnt, int *matind, double *matval);
 
 #endif
