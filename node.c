a3ca8320c848650f5b94050ea605c1c7cd5a599a
Amir Moualem <amoualem@gmail.com>
2012-08-27 19:01:31 +0300

    Only count edges once

--- node.c
+++ node.c
@@ -98,8 +98,11 @@ bool add_one_edge(node* nodeFrom, node* nodeTo, double weight) {
 		perror(ERROR_MALLOC);
 		return FALSE;
 	}
-/*TODO: change this... the problem is that this function is called twice for each one edge*/
-	edges[(edgesAdded++) / 2] = *newEdge;
+
+	/* add edge to array (only once) */
+	if (newEdge->nodeTo < newEdge->nodeFrom) {
+		edges[edgesAdded++] = *newEdge;
+	}
 	return TRUE;
 }
 
